0662dbabe6b4e25b55223eae0c7187dceb1a0fc6
Vlad Shynder <shynderv@gmail.com>
2020-10-28 01:41:47 +0200

    Updates

---
+++ precache-manifest.505b9fe019bbb578b7696cb807268a51.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d46874216b8205415b2dd35f2df7cf",
+    "url": "/full-stack-developer_portfolio/index.html"
+  },
+  {
+    "revision": "df8d2935d536c2ba0ea5",
+    "url": "/full-stack-developer_portfolio/static/css/main.0b76dd81.chunk.css"
+  },
+  {
+    "revision": "757bc9a5b9e89b2d88ef",
+    "url": "/full-stack-developer_portfolio/static/js/2.b9d23b58.chunk.js"
+  },
+  {
+    "revision": "8bcdfbedbaedaebc1be8002ea83beeb7",
+    "url": "/full-stack-developer_portfolio/static/js/2.b9d23b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8d2935d536c2ba0ea5",
+    "url": "/full-stack-developer_portfolio/static/js/main.3b32a417.chunk.js"
+  },
+  {
+    "revision": "79befc5cd290e98076c7",
+    "url": "/full-stack-developer_portfolio/static/js/runtime-main.12ad08df.js"
+  },
+  {
+    "revision": "2b3dd0dac504bdac8b5a8bc06bc7cd06",
+    "url": "/full-stack-developer_portfolio/static/media/chuck-norris.2b3dd0da.png"
+  },
+  {
+    "revision": "893359f132afda9b52063329bd419f65",
+    "url": "/full-stack-developer_portfolio/static/media/heroku-todo.893359f1.png"
+  },
+  {
+    "revision": "81a4c771c25726fce67e88043a751558",
+    "url": "/full-stack-developer_portfolio/static/media/image-finder.81a4c771.png"
+  },
+  {
+    "revision": "8c78cbfa71bc2146e0e7301d0b28096e",
+    "url": "/full-stack-developer_portfolio/static/media/kinoteka.8c78cbfa.png"
+  },
+  {
+    "revision": "6b3fbed1b47a9ebeb4e8c1950d23a898",
+    "url": "/full-stack-developer_portfolio/static/media/messenger-clone-back.6b3fbed1.png"
+  },
+  {
+    "revision": "0675e62508ae088952bd7418fc55b6bf",
+    "url": "/full-stack-developer_portfolio/static/media/messenger-clone-front.0675e625.png"
+  },
+  {
+    "revision": "ee747638b6feba1e0195aa767529f9ce",
+    "url": "/full-stack-developer_portfolio/static/media/movie-finder.ee747638.png"
+  },
+  {
+    "revision": "1b84a465815fa13932c060177453c2a4",
+    "url": "/full-stack-developer_portfolio/static/media/phone-book.1b84a465.png"
+  },
+  {
+    "revision": "82b87600b16bc26ed776da7f1a206404",
+    "url": "/full-stack-developer_portfolio/static/media/photo-avatar.82b87600.png"
+  },
+  {
+    "revision": "35e3b43c624585033b40c0052188d4bd",
+    "url": "/full-stack-developer_portfolio/static/media/react-todo.35e3b43c.png"
+  },
+  {
+    "revision": "65a064e5ba5e2e54eae8a54ad8ef035b",
+    "url": "/full-stack-developer_portfolio/static/media/rsp-game.65a064e5.png"
+  }
+]);
